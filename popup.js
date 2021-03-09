c4a8cdfbceb963636d8548d04a67044c97971b8d
AntoRin <antorinish@gmail.com>
2021-03-09 15:09:27 +0530

    Completed basic objectives

--- popup.js
+++ popup.js
@@ -2,9 +2,7 @@ let extBtns = document.querySelectorAll(".init");
 
 extBtns.forEach(btn => {
   btn.onclick = () => {
-    console.log("from popup");
     chrome.tabs.query({ active: true, currentWindow: true }, tab => {
-      console.log(tab[0]);
       chrome.tabs.sendMessage(tab[0].id, btn.id);
     });
   };
